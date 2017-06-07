05a34709f043f0aeb0626c58ea49f69ededf8f03
Abhinickz <Abhinickz6@gmail.com>
2017-06-07 15:41:28 +0530

    Added bash color support for Output & Errors.
    Fixed: Colors in warnings and errros returned by gcc compiler.

--- run.sh
+++ run.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+code=$(g++ /home/Abhishek.Bhasker/c_plus_plus/stackTest.cpp -o /home/Abhishek.Bhasker/c_plus_plus/program_c_plus_plus.exe 2>&1);
+if [ -z "${code// }" ]
+then
+    tput setaf 2;
+        echo -e "========================= OUTPUT ========================="
+    tput sgr0;
+	/home/Abhishek.Bhasker/c_plus_plus/program_c_plus_plus.exe
+else
+    tput setaf 1;
+        echo -e "========================= ERROR =========================\n"
+    tput sgr0;
+    # Because I wasn't getting colored error ouput and created the another alias to find errors :)
+	`g++ /home/Abhishek.Bhasker/c_plus_plus/stackTest.cpp -o /home/Abhishek.Bhasker/c_plus_plus/program_c_plus_plus.exe`
+fi
+
+
+
+
+# Line Commented for another configuration.
+
+<<comment
 code=$(g++ /cygdrive/d/Abhasker/Per/clion_C_plus_plus/main.cpp -o main.exe 2>&1);
 if [ -z "${code// }" ]
 then
@@ -6,3 +28,5 @@ then
 else
 	echo -e "++++++++++++++++++++ Error: ++++++++++++++++++++\n$code"
 fi
+
+comment
